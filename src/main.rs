618be682dc235f4bd37119d7f6c04a83d528bba4
pavles6 <pavles6@protonmail.com>
2023-07-21 16:17:35 +0200

    single threaded http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Listening on port 8080");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_conn(stream);
+    }
+}
+
+fn handle_conn(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    let req_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if req_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}",);
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
